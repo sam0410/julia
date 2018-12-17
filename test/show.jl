1ca94064898071f0adfd279e0e62029b3f9c77f3
sam0410 <samikshya.chand.ece15@iitbhu.ac.in>
2018-12-18 05:12:17 +0530

    fix display of operators in a pair

--- test/show.jl
+++ test/show.jl
@@ -994,6 +994,9 @@ end
     # issue #28327
     d = Dict(Pair{Integer,Integer}(1,2)=>Pair{Integer,Integer}(1,2))
     @test showstr(d) == "Dict((1=>2)=>(1=>2))" # correct parenthesis
+    
+    d = Dict((+)=>1)
+    @test showstr(d) == "Dict((+)=>1)" # (#29536)
 end
 
 @testset "alignment for pairs" begin  # (#22899)
